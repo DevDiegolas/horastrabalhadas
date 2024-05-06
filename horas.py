15b10bdac22a483f38fc2df3060829dd5ecb0dec
Diego <diegogpssth@gmail.com>
2024-05-06 17:15:15 -0300

    colocando o salvo pq eu tinha esquecido :(

---
+++ horas.py
@@ -0,0 +1,25 @@
+#Guardando quantas aulas de cada dia
+sabado = int(input("Coloque quantos sábados deu aula: "))
+segunda = int(input("Coloque quantas segundas deu aula: "))
+quarta = int(input("Coloque quantas quartas deu aula: "))
+sexta = int(input("Coloque quantas sextas deu aula: "))
+
+#Conta utilizando como base o site 
+sabadoaula = sabado * 1.50
+segundaaula = segunda * 1.50
+quartaaula = quarta * 1.50
+sextaaula = sexta * 1.50
+
+#Contas utilizando o site como base
+totalaulas = segunda + quarta + sexta + sabado
+totalhoras = sabadoaula + segundaaula + quartaaula + sextaaula
+total = totalaulas * 37.50
+
+#Transformando em inteiro
+total = int(total)
+totalhoras = int(totalhoras)
+
+#Printando tudo
+print("O total de horas trabalhadas é: ", totalhoras)
+print("O total de aulas é: ", totalaulas)
+print("O total de dinheiro recebido é: ", total)
